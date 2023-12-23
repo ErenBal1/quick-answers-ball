4f0d2b40d8ce71dcf9ac6c7ac54663f25d24680b
ErenBal <erenbal.academic@gmail.com>
2023-12-24 02:36:31 +0300

    Initial Commit

---
+++ QuickAnswerBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickAnswerBall
+//
+//  Created by Eren Bal on 24.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
